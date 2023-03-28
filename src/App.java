35d5d79436b53ee467c43cf6f398239bdafbdc3f
yamilemlf <yamilemlessa@gmail.com>
2023-03-28 12:46:47 -0300

    iniciando projeto java, já adicionando a conexão http com a api de filmes

---
+++ src/App.java
@@ -0,0 +1,17 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        HttpClient client = HttpClient.newHttpClient();
+        URI endereco = URI.create(url);
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
+    }
+}
